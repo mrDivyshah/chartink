e061a57cc6034c1d9a5b7491e1ffce2cc33b9cd6
mrDivyshah <105691678+mrDivyshah@users.noreply.github.com>
2024-12-14 14:18:42 +0530

    first commit

--- method.py
+++ method.py
@@ -8,6 +8,7 @@ def web_driver():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     driver = webdriver.Chrome(options=options)
+    
     return driver
 
 def get_url_and_index(driver):
