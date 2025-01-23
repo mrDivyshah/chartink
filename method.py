47b3af0621d1d005ca616a0cf93b401fd3492cb2
DIVY SHAH <105691678+mrDivyshah@users.noreply.github.com>
2025-01-23 10:30:18 +0530

    Update method.py
    retrun

--- method.py
+++ method.py
@@ -7,8 +7,10 @@ def is_colab():
 subprocess.run(["pip", "install", "selenium"], check=True)
 subprocess.run(["apt-get", "update"], check=True)
 subprocess.run(["apt-get", "install", "-y", "chromium-browser"], check=True)
+
 subprocess.run(["apt-get", "install", "-y", "chromium"], check=True)
 subprocess.run(["apt-get", "install", "-y", "chromium-driver"], check=True)
+
 subprocess.run(["pip", "install", "webdriver-manager", "Pillow", "reportlab"], check=True)
 subprocess.run(["pip", "install", "python-telegram-bot"], check=True)
 from google.colab import output, files
